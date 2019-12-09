112dee404c2af1e275bb7d4bbad49a8dff28063a
Nolelle <eyu1@ualberta.ca>
2019-12-09 14:30:03 -0500

    Update game.js

--- game.js
+++ game.js
@@ -6,16 +6,55 @@
         return moves[randNum];
     }
 
-    console.log(computerPlay())
+    
+function playRound(caseInsensitive, computerSelection) {
+    caseInsensitive = caseInsensitive.toLowerCase(); 
+    playerWins = 'Player is the winner'
+    compWins = 'Computer is the winner'
+    tie = "Tie! Play again!"
+    switch(computerSelection) {
+        case 'Rock':
+          if (caseInsensitive = 'Rock') {
+              return tie
 
-function playRound(playerSelection, computerSelection) {
-	if (playerSelection = 'Rock' && computerSelection = 'Paper') {
-        let winner = 'Computer Wins!'
-    } else if (playerSelection = 'paper' && computerSelection = 'Scissors') {
-        let winner
-    }
+          } else if (caseInsensitive = 'Paper') {
+              return playerWins
+              
+          } else {
+              console.log('Computer is the winner!')
+              return compWins
+          }
+          break;
+        case 'Paper':
+            if (caseInsensitive = 'Rock') {
+                return compWins
+              } else if (caseInsensitive = 'Paper') {
+                return tie
+              } else {
+                return playerWins
+              }
+          break;
+        case 'Scissors':
+            if (caseInsensitive = 'Rock') {
+             return playerWins
+              } else if (caseInsensitive = 'Paper') {
+                 
+              } else {
+                return tie
+              }
+
+          break;
+      }
+
+      function game() {
+
+        for (let i = 0; i < 6; i++) {
+
+          let choice = [];
+            choice[i]= prompt("Player please enter your first choice? Rock, Paper, or Scissors?")
+            result = playRound(choice[i],computerSelection);
+        }
+        return result;
+      }
 }
 
-const playerSelection = 'Rock'
-const computerSelection = computerPlay()
-console.log(playRound(playerSelection, computerSelection))
