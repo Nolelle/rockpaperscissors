8a6be99da43f3f42ba168fc9409ba12957bdbf34
Nolelle <eyu1@ualberta.ca>
2019-12-16 18:12:41 -0700

    working

--- game.js
+++ game.js
@@ -1,68 +1,63 @@
-    function computerPlay() {
-
-        let randNum = Math.floor(Math.random() * 3);
-        let moves = ['Rock', 'Paper', 'Scissors'];
-
-        return moves[randNum];
+function computerPlay() {
+  let randNum = Math.floor(Math.random() * moves.length);
+  const moves = ['Rock', 'Paper', 'Scissors'];
+  
+  return moves[randNum];
     }
-
-    
-function playRound(caseInsensitive, computerSelection) {
-    caseInsensitive = caseInsensitive.toLowerCase(); 
-    playerWins = 'Player is the winner'
-    compWins = 'Computer is the winner'
+function playRound(playerSelection, computerSelection) {
+    caseInsensitive = playerSelection.toLowerCase(); 
+    playerWins = 'Player is the winner';
+    compWins = 'Computer is the winner';
     tie = "Tie! Play again!"
+
     switch(computerSelection) {
-        case 'Rock':
-          if (caseInsensitive = 'Rock') {
-              return tie
-
-          } else if (caseInsensitive = 'Paper') {
-              return playerWins
-              
-          } else {
-              console.log('Computer is the winner!')
-              return compWins
-          }
-          break;
-        case 'Paper':
-            if (caseInsensitive = 'Rock') {
-                return compWins
-              } else if (caseInsensitive = 'Paper') {
-                return tie
-              } else {
-                return playerWins
-              }
-          break;
-        case 'Scissors':
-            if (caseInsensitive = 'Rock') {
-             return playerWins
-              } else if (caseInsensitive = 'Paper') {
-                 
-              } else {
-                return tie
-              }
-
-          break;
-      }  
+      case 'Rock':
+        if (caseInsensitive === 'Rock') {
+            return tie;
+  
+        } else if (caseInsensitive === 'Paper') {
+            return playerWins;
+            
+        } else {
+            return compWins;
+        }
+        break;
+      case 'Paper':
+          if (caseInsensitive === 'Rock') {
+              return compWins;
+            } else if (caseInsensitive === 'Paper') {
+              return tie;
+            } else {
+              return playerWins;
+            }
+        break;
+      case 'Scissors':
+          if (caseInsensitive === 'Rock') {
+              return playerWins;
+            } else if (caseInsensitive === 'Paper') {
+               return compWins;
+            } else {
+              return tie;
+            }
+  
+        break;
+    }  
+   
 }
 
 function game() {
-
   for (let i = 0; i < 6; i++) {
 
-    let choice =  window.prompt("Player please enter your choice? Rock, Paper, or Scissors?")
-    
-    let result= playRound(choice[i],computerSelection);
   }
   return result;
 }
 
 
-game();
+const buttons = document.querySelectorAll('button');
 
-
-
+buttons.forEach(button => { 
+  button.addEventListener('click', playRound);
+});
 
 
 
